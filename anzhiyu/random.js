658bf69593ce3bfc4e22c6e91cc967914b976b65
平泽唯的小吉他 <1968870316@qq.com>
2024-07-30 18:44:38 +0800

    Site updated: 2024-07-30 18:44:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/29/hello-world/"];function toRandomPost(){
+var posts=["2024/07/29/hello-world/","2024/07/30/Anzhiyu站点配置/","2024/07/30/markdown格式简要说明/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
